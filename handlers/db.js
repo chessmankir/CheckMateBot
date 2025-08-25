334488d6dee90cc9fd14b51aa897111edd1d36f2
Kir Skornyakov <44677466-skornkir@users.noreply.replit.com>
2025-08-25 16:53:29 +0000

    url

--- handlers/db.js
+++ handlers/db.js
@@ -1,7 +1,8 @@
     // db.js
     const { Pool } = require('pg');
     require('dotenv').config();
-
+    console.log('process.env.SUPABASE_DATABASE_URL');
+    console.log(process.env.SUPABASE_DATABASE_URL);
     const pool = new Pool({
       connectionString: process.env.SUPABASE_DATABASE_URL,
       ssl: { rejectUnauthorized: false },
